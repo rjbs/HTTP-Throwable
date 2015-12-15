d463e41a49297319883cd40ce36d0ffd3bb59db9
Chris Prather <chris@prather.org>
2015-12-15 13:49:26 -0600

    add role for 429: Too Many Requests

---
+++ lib/HTTP/Throwable/Role/Status/TooManyRequests.pm
@@ -0,0 +1,26 @@
+package HTTP::Throwable::Role::Status::TooManyRequests;
+
+use Moo::Role;
+
+with(
+    'HTTP::Throwable',
+    'HTTP::Throwable::Role::BoringText',
+);
+
+sub default_status_code { 429 }
+sub default_reason      { 'Too Many Requests' }
+
+no Moo::Role; 1;
+
+__END__
+
+# ABSTRACT: 404 Not Found
+
+=head1 DESCRIPTION
+
+The 429 status code indicates that the user has sent too many
+requests in a given amount of time ("rate limiting"). The response
+representations SHOULD include details explaining the condition,
+and MAY include a Retry-After header indicating how to wait before
+making a new request.
+
